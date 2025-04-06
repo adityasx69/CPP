fe758c39617e060840de96a24eca4ceefaede82e
Aditya Saraswat <saraswataditya664@gmail.com>
2025-04-06 17:13:42 +0530

    Add files via upload

---
+++ 05.conditionals.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a;
+    // cin>>a;
+    // if(a>0){
+    //     cout<<"a is positive"<<endl;
+    // }
+    // else{
+    //     cout<<"a is negative";
+    // }
+    // int a,b;
+    // cin>>a;
+    // cin>>b;
+    // if(a>b){
+    //     cout<<"a is greater than b";
+    // }
+    // else{
+    //     cout<<"b is greater than a";
+    // }
+    int a;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    if(a==0){
+        cout<<"a is 0"<<endl;
+    }
+    if(a<0){
+        cout<<"a is negative"<<endl;
+    }
+    if(a>0){
+        cout<<"a is positive"<<endl;
+    }
+}
